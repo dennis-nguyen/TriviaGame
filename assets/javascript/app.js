382b58e13c1678e41d8bb5dc0fa91d1d9c606921
dennis-nguyen <nguyen.denn@gmail.com>
2017-04-21 02:09:20 -0700

    Changed Logic, Pulls Trivia Questions and Gifs from API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,132 +5,63 @@ var unansweredCounter = 0;
 var timer = 20;
 var totalTimer = timer;
 var qTimer;
-
-var questions = [
-
-    {
-        question: "Which country did Bibimbap originate from?",
-        answer: "Korea",
-        choice1: "Japan",
-        choice2: "Thailand",
-        choice3: "Singapore",
-        picture: "bibimbap.jpg"
-    }, {
-        question: "Which country did Chicken Satay originate from?",
-        answer: "Indonesia",
-        choice1: "Malaysia",
-        choice2: "Philippines",
-        choice3: "Thailand",
-        picture: "chickensatay.jpg"
-    }, {
-        question: "Which country did this broken rice dish originate from?",
-        answer: "Vietnam",
-        choice1: "Singapore",
-        choice2: "Thailand",
-        choice3: "Cambodia",
-        picture: "comtam.jpg"
-    }, {
-        question: "Where did Dim Sum originate from?",
-        answer: "Taiwan",
-        choice1: "Hong Kong",
-        choice2: "Southern California",
-        choice3: "Singapore",
-        picture: "dimsum.jpg"
-    }, {
-        question: "What raw meat is commonly eaten in Japan?",
-        answer: "Horse",
-        choice1: "Veal",
-        choice2: "Sheep",
-        choice3: "Lamb",
-        picture: "meat.jpg"
-    }, {
-        question: "Hainan Chicken is considered one of the national dishes of which country?",
-        answer: "Singapore",
-        choice1: "China",
-        choice2: "Thailand",
-        choice3: "Cambodia",
-        picture: "hainanchicken.jpg"
-    }, {
-        question: "Around how many different variations of Kimchi are there in Korea?",
-        answer: "200",
-        choice1: "150",
-        choice2: "100",
-        choice3: "50",
-        picture: "kimchi.jpg"
-    }, {
-        question: "Where do people eat Natto, fermented beans, for breakfast?",
-        answer: "Japan",
-        choice1: "Malaysia",
-        choice2: "Hong Kong",
-        choice3: "Vietnam",
-        picture: "natto.jpg"
-    }, {
-        question: "What are the rice balls from Japan called?",
-        answer: "Onigiri",
-        choice1: "Baogiri",
-        choice2: "Banh Bao",
-        choice3: "Irigino",
-        picture: "onigiri.jpg"
-    }, {
-        question: "Where did Pho originate from?",
-        answer: "Vietnam",
-        choice1: "China",
-        choice2: "Thailand",
-        choice3: "Indonesia",
-        picture: "pho.jpg"
-    }, {
-        question: "Where did Shabu Shabu originate from?",
-        answer: "Japan",
-        choice1: "Taiwan",
-        choice2: "Thailand",
-        choice3: "Indonesia",
-        picture: "shabu.jpg"
-    }, {
-        question: "Sukiyaki originated from which country?",
-        answer: "Japan",
-        choice1: "Cambodia",
-        choice2: "South Korea",
-        choice3: "Thailand",
-        picture: "sukiyaki.jpg"
-    }, {
-        question: "What is this sour and spicy soup called?",
-        answer: "Tom Yum",
-        choice1: "Yum Tom",
-        choice2: "Ting Ling",
-        choice3: "Yum Ting",
-        picture: "thaitomyum.jpg"
-    }
-
-];
+var triviaQ;
+var images;
+var gameStart = false;
 
 $(document).ready(function() {
 
+//GRABS THE TRIVIA QUESTIONS
+    function whichGame() {
+        $.ajax({
+            url: "https://opentdb.com/api.php",
+            data: {
+                amount: 9,
+                category: 27,
+                difficulty: 'medium',
+                type: 'multiple'
+            },
+            method: "GET"
+        }).done(function(response) {
+
+            triviaQ = response;
+            getImage();
+
+        });
+    }
+//GRABS A GIF BASED ON THE TRIVIA QUESTIONS ANSWER
+    function getImage() {
+        var currentQ = triviaQ.results[qCounter].correct_answer.replace(/\W+/g, " ").split(" ").join("+");
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=" + currentQ + "&rating=pg&api_key=dc6zaTOxFJmzC",
+            method: "GET"
+        }).done(function(response2) {
+            images = response2;
+
+            if (gameStart === false) {
+                gameStart = true;
+                start();
+            }
+
+        });
+    }
+
     function appendQuestion() {
         $('#question').typeIt({
-            strings: questions[qCounter].question,
-            speed: 75,
+            strings: triviaQ.results[qCounter].question,
+            speed: 50,
             autoStart: false,
             cursor: false,
             lifeLike: true
         });
-        $("#answer").html(questions[qCounter].answer);
-        $("#choice1").html(questions[qCounter].choice1);
-        $("#choice2").html(questions[qCounter].choice2);
-        $("#choice3").html(questions[qCounter].choice3);
-        $('#picture').css({ "background-image": "url(assets/images/" + questions[qCounter].picture + ")", "background-position": "cover" });
-    }
-    //Fisher-Yates Shuffle
-    function randomizeQuestions(array) {
-        var currentIndex = array.length,
-            temporaryValue, randomIndex;
-        while (0 !== currentIndex) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-        return array;
+        $("#answer").html(triviaQ.results[qCounter].correct_answer);
+        $("#choice1").html(triviaQ.results[qCounter].incorrect_answers[0]);
+        $("#choice2").html(triviaQ.results[qCounter].incorrect_answers[1]);
+        $("#choice3").html(triviaQ.results[qCounter].incorrect_answers[2]);
+        $('#picture').css({
+            "background-image": "url(" + images.data[0].images.downsized.url + ")",
+            "background-position": "cover"
+        });
     }
 
     function randomizeChoices() {
@@ -165,7 +96,10 @@ $(document).ready(function() {
     function transition() {
         $("#picture").hide();
         $(".choices").hide();
-        $("#showAnswer").html(questions[qCounter].answer).typeIt({ speed: 100, lifeLike: true });
+        $("#showAnswer").html(triviaQ.results[qCounter].correct_answer).typeIt({
+            speed: 100,
+            lifeLike: true
+        });
         clearInterval(qTimer);
         nextQuestionTimer();
     }
@@ -173,20 +107,21 @@ $(document).ready(function() {
     function nextQuestionTimer() {
         qCounter++;
         console.log(qCounter);
-        if (qCounter != questions.length) {
+        if (qCounter != 9) {
+            getImage();
             setTimeout(function() {
                 $("#result").empty();
                 $("#picture").show();
                 nextQuestion();
                 resetTimer();
-            }, 4000);
+            }, 3000);
         } else {
-
             setTimeout(function() {
                 $("#result").empty();
                 finalScreen();
-            }, 4000);
+            }, 3000);
         }
+
     }
 
     function nextQuestion() {
@@ -209,31 +144,32 @@ $(document).ready(function() {
     }
 
     function finalScreen() {
-        if (qCounter == questions.length) {
-            $('#question').typeIt({
-                strings: "Final Score",
-                speed: 75,
-                autoStart: false,
-                cursor: false,
-                lifeLike: true
-            });
-            $('#result').typeIt({
-                strings: ["Correct Answers: " + correctCounter, "Incorrect Answers: " + incorrectCounter, "Unanswered Questions: " + unansweredCounter],
-                speed: 50,
-                autoStart: false,
-                lifeLike: true
-            });
-            $("#result").append("<br> <img src='assets/images/dwight.gif'>")
-            $("#result").append("<br> <button id='reset' class='btn-lg btn-danger'>New Game</button>")
-            $("#reset").click(function() {
-                resetGame();
-            });
-        }
+        qCounter = 0;
+        console.log(qCounter);
+        getImage();
+        $('#question').typeIt({
+            strings: "Final Score",
+            speed: 75,
+            autoStart: false,
+            cursor: false,
+            lifeLike: true
+        });
+        $('#result').typeIt({
+            strings: ["Correct Answers: " + correctCounter, "Incorrect Answers: " + incorrectCounter, "Unanswered Questions: " + unansweredCounter],
+            speed: 50,
+            autoStart: false,
+            lifeLike: true
+        });
+        $("#result").append("<br> <img src='assets/images/dwight.gif'>");
+        $("#result").append("<br> <button id='reset' class='btn-lg btn-danger'>New Game</button>");
+        $("#reset").click(function() {
+            resetGame();
+        });
     }
 
     function resetTimer() {
         timer = 20;
-        qTimer = setInterval(questionCountDown, 1000);
+        qTimer = setInterval(questionCountDown, 3000);
     }
 
     function resetGame() {
@@ -246,30 +182,29 @@ $(document).ready(function() {
         $("#result").empty();
         $("#picture").show();
         $(".choices").show();
-        randomizeQuestions(questions);
         appendQuestion();
         resetTimer();
         questionCountDown();
     }
 
-    function applyClickHandlers() {
+    function applyClickHandlers(triviaQ) {
         $(".wrongAnswer").click(function() {
-            wrongAnswer();
+            wrongAnswer(triviaQ);
         });
         $("#answer").click(function() {
-            rightAnswer();
+            rightAnswer(triviaQ);
         });
     }
 
     function start() {
-        randomizeQuestions(questions);
+
         appendQuestion();
         applyClickHandlers();
         randomizeChoices();
         resetTimer();
         questionCountDown();
-        finalScreen();
+        getImage();
     }
 
-    start();
+    whichGame();
 });
